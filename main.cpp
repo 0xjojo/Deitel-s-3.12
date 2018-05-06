62e702eb13fbce0ae2d84c91d1c7a268690108e7
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2018-05-06 23:07:06 +0200

    Deitel's 3.12
    Deitel's 3.12

---
+++ main.cpp
@@ -0,0 +1,83 @@
+/*3.12 (Account Class) Create an Account class that a bank might use to represent customers’ bank
+accounts. Include a data member of type int to represent the account balance. [Note: In subsequent
+chapters, we’ll use numbers that contain decimal points (e.g., 2.75)—called floating-point values—
+to represent dollar amounts.] Provide a constructor that receives an initial balance and uses it to
+ initialize the data member. The constructor should validate the initial balance to ensure that it’s greater
+than or equal to 0. If not, set the balance to 0 and display an error message indicating that the initial
+balance was invalid. Provide three member functions. Member function credit should add an
+amount to the current balance. Member function debit should withdraw money from the Account
+and ensure that the debit amount does not exceed the Account’s balance. If it does, the balance
+should be left unchanged and the function should print a message indicating "Debit amount exceeded account
+ balance." Member function getBalance should return the current balance. Create a
+program that creates two Account objects and tests the member functions of class Account.*/
+#include <iostream>
+
+using namespace std;
+class Account
+{
+public:
+    Account(int b)
+    {
+        if(b < 0)
+        {
+            balance = 0;
+            cout << "Error: The initial balance was invalid." << endl;
+        }
+        else
+        {
+            balance = b;
+        }
+    }
+
+
+    void credit(int c)
+    {
+        if(c > 0)
+        {
+            balance += c;
+        }
+    }
+
+    void debit(int d)
+    {
+        if(d > 0)
+        {
+            if(d > balance)
+            {
+                cout << "Error: Debit amount exceeded account balance." << endl;
+            }
+            else
+            {
+                balance -= d;
+            }
+        }
+    }
+
+    int getBalance()
+    {
+        return balance;
+    }
+
+private:
+    int balance;
+
+};
+
+int main ()
+{
+
+
+    Account a(1000);
+    Account b(-20);
+
+    a.credit();
+
+    a.debit();
+
+    b.credit();
+
+    b.debit();
+
+    cout << "Account A balance is: " << a.getBalance() << endl;
+    cout << "Account B balance is: " << b.getBalance() << endl;
+}
